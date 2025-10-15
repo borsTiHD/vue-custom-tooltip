36b9a6b886087d669efef861fc7ff25577e7bf71
Bastian Jakobs <bastian.jakobs@telekom.de>
2025-10-15 10:33:05 +0200

    feat: add tailwindcss as a dependency in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -8,6 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     vueDevTools(),
   ],
   resolve: {
