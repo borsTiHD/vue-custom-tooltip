d0ed3c79538be1add33079391ac6c5ed2c085d03
Bastian Jakobs <bastian.jakobs@telekom.de>
2025-10-15 12:31:39 +0200

    refactor: remove unused tooltipDirective export and streamline directive usage

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import type { App, Plugin } from 'vue'
 import Tooltip from './components/tooltip/Tooltip.vue'
-import { tooltipDirective, vTooltip } from './directives/tooltip'
+import { vTooltip } from './directives/tooltip'
 
 // Export components and directives
-export { Tooltip, tooltipDirective, vTooltip }
+export { Tooltip, vTooltip }
 
 // Export types
 export type { TooltipProps, TooltipSlots } from './components/tooltip/Tooltip.vue'
@@ -12,7 +12,7 @@ export type { TooltipProps, TooltipSlots } from './components/tooltip/Tooltip.vu
 export const VueCustomTooltip: Plugin = {
   install(app: App) {
     app.component('Tooltip', Tooltip)
-    app.directive('tooltip', tooltipDirective)
+    app.directive('tooltip', vTooltip)
   },
 }
 
