989115e396a63b8f3ba1ad4095ed4cf28b79c3cc
Bastian Jakobs <bastian.jakobs@telekom.de>
2025-10-15 10:13:44 +0200

    fix: reorder imports and fix formatting in various configuration files

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,7 @@
-import { globalIgnores } from 'eslint/config'
+import pluginVitest from '@vitest/eslint-plugin'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
+import { globalIgnores } from 'eslint/config'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -18,7 +18,7 @@ export default defineConfigWithVueTs(
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
