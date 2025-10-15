3c1f7f4a8d91c6daba8929766bd6e70409bf3f8a
Bastian Jakobs <bastian.jakobs@telekom.de>
2025-10-15 13:19:20 +0200

    refactor: remove vue-router dependency and related usage from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import directives from '@/directives'
 import App from './App.vue'
-import router from './router'
 import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(router)
 app.use(directives)
 
 app.mount('#app')
